eddbae9d89d91979634d79df5089b187a775b3e0
konstantin <kostya.ignatev.14@mail.ru>
2024-04-19 16:46:12 +0300

    Инициализирован проек

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    components: {  },
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        
+    },
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
